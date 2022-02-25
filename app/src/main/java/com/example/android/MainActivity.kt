2e199c8416d53ce77ba891b404ee0aa920fa88eb
arhpov035 <78172721+arhpov035@users.noreply.github.com>
2022-02-26 00:43:51 +0300

    Урок 13; 23:36. Не рабочий скрипт. Нужно поменять id элементам

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -8,9 +8,8 @@ import com.example.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var bindingClass : ActivityMainBinding
-    val a = 324
-    val b = 34
-    var resultValue = ""
+
+
 
     override fun onCreate(s: Bundle?) {
         super.onCreate(s)
@@ -19,61 +18,13 @@ class MainActivity : AppCompatActivity() {
 
         bindingClass.btYes.setOnClickListener{
 
-            resultValue = bindingClass.textShow.text.toString()
+//            var resultValue = bindingClass.edVa
 
             when(resultValue){
-                in "Хоченьш я угаданю сколько тебе лет?" ->{
-                    bYes("Тебе 8 лет")
 
-                }
-                in "Хочешь я угадаю, как тебя зовут?" ->{
-                    val tempText = "Тебя зовут ${Constance.NAME_SYN}"
-                    bYes(tempText)
-                }
-                in "Хочешь я угодаю, как зовут твою маму?" ->{
-                    bYes("Твою маму зовут Анна. Она любит покушать.")
-                }
-                in "Хочешь я угодаю, как зовут твого папу?" ->{
-                    bYes("Твоего папу зовут Виталий. У него спортивное телосложение.")
-                }
-                in "Хочешь я угодаю, в каком классе ты учишся?" ->{
-                    bYes("Ты учишься в первом классе через дорогу от школы")
-                    bindingClass.btNext.visibility = View.GONE
-                }
             }
         }
 
-        bindingClass.btNext.setOnClickListener{
-            when(resultValue){
-                in "Тебе 8 лет" ->{
-                    bNext("Хочешь я угадаю, как тебя зовут?")
-                }
-                in "Тебя зовут Витя Архипов" ->{
-                    bNext("Хочешь я угодаю, как зовут твою маму?")
-                }
-                in "Твою маму зовут Анна. Она любит покушать." ->{
-                    bNext("Хочешь я угодаю, как зовут твого папу?")
-                }
-                in "Твоего папу зовут Виталий. У него спортивное телосложение." ->{
-                    bNext("Хочешь я угодаю, в каком классе ты учишся?")
-                }
-            }
-        }
 
-    }
 
-    fun bYes(result:String){
-        resultValue = result
-        bindingClass.textShow.text = resultValue
-        bindingClass.btYes.visibility = View.GONE
-        bindingClass.btNo.visibility = View.GONE
-        bindingClass.btNext.visibility = View.VISIBLE
-    }
-    fun bNext(result:String){
-        resultValue = result
-        bindingClass.textShow.text = resultValue
-        bindingClass.btNext.visibility = View.GONE
-        bindingClass.btYes.visibility = View.VISIBLE
-        bindingClass.btNo.visibility = View.VISIBLE
-    }
 }
--- app/src/main/java/com/example/android/constance/Constance.kt
+++ app/src/main/java/com/example/android/constance/Constance.kt
@@ -1,6 +1,25 @@
 package com.example.android.constance
 
-object Constance{
-    const val NAME_SYN = "Витя Архипов"
-    const val NAME_SUPRUGA = "Анна"
+object R{
+    object drawable{
+        val face_co_1 = 123456
+        val face_co_2 = 123457
+        val face_co_3 = 123458
+        val face_co_4 = 123459
+    }
+    object layout{
+        val activity_main = 1234510
+    }
+    object mipmap{
+
+    }
+    object colors{
+
+    }
+    object strings{
+
+    }
+    object style{
+
+    }
 }
