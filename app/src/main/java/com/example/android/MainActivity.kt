1a5734bdeec7fc78c2691e34104b134c621a62da
arhipov035 <arhipov035@gmail.com>
2022-02-26 21:43:36 +0300

    13 урок пример с картинками

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.example.android.constance.Constance
 //import com.example.android.constance.Constance
 import com.example.android.databinding.ActivityMainBinding
 
@@ -16,13 +18,50 @@ class MainActivity : AppCompatActivity() {
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
-        bindingClass.btYes.setOnClickListener {
+        bindingClass.btResult.setOnClickListener{
 
-//            var resultValue = bindingClass.edVa
+            Log.d("MyLog", "ID egor = ${R.drawable.egor}")
 
-//            when(resultValue){
-//
-//            }
+            val resultValue = bindingClass.tvName.text.toString()
+            bindingClass.imPhoto.visibility = View.VISIBLE
+
+            when(resultValue){
+                Constance.DIRECTOR -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+
+                    val tempText = "Получите Ваши ${Constance.DIRECTOR_SUELDO}"
+                    if (bindingClass.edPassword.text.toString() == Constance.DIRECTOR_PASSWORD){
+                        bindingClass.tvResult.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.egor)
+                    }else{
+                        bindingClass.tvResult.text = "Неверный пароль"
+                    }
+                }
+
+                Constance.INGINER -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    val tempText = "Получите Ваши ${Constance.INGINER_SUELDO}"
+                    if (bindingClass.edPassword.text.toString() == Constance.INGINER_PASSWORD){
+                        bindingClass.tvResult.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.andrey)
+                    }else{
+                        bindingClass.tvResult.text = "Неверный пароль"
+                    }
+                }
+
+                Constance.DVORNIK -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    val tempText = "Получите Ваши ${Constance.DVORNIK_SUELDO}"
+                    if (bindingClass.edPassword.text.toString() == Constance.DVORNIK_PASSWORD){
+                        bindingClass.tvResult.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.sergey)
+                    }else{
+                        bindingClass.tvResult.text = "Неверный пароль"
+                    }
+                }
+            }
+
+
         }
 
     }
--- app/src/main/java/com/example/android/constance/Constance.kt
+++ app/src/main/java/com/example/android/constance/Constance.kt
@@ -1,25 +1,15 @@
 package com.example.android.constance
 
-object R{
-    object drawable{
-        val face_co_1 = 123456
-        val face_co_2 = 123457
-        val face_co_3 = 123458
-        val face_co_4 = 123459
-    }
-    object layout{
-        val activity_main = 1234510
-    }
-    object mipmap{
+object Constance{
+    const val INGINER_SUELDO = 10000
+    const val DVORNIK_SUELDO = 1000
+    const val DIRECTOR_SUELDO = 100000
 
-    }
-    object colors{
+    const val DIRECTOR = "Egor"
+    const val INGINER = "Andrey"
+    const val DVORNIK = "Sergey"
 
-    }
-    object strings{
-
-    }
-    object style{
-
-    }
+    const val INGINER_PASSWORD = "1232"
+    const val DIRECTOR_PASSWORD = "4565"
+    const val DVORNIK_PASSWORD = "6677"
 }
