b330d26e12c2ace2031f76d5230cb330ef874c40
arhpov035 <78172721+arhpov035@users.noreply.github.com>
2022-03-14 01:16:20 +0300

    Урок 17 передача данных между Activity с помощью Intent и принятие отправленные данные с помощью метода Activity onActivityResult.

--- app/src/main/java/com/example/android/TestActivity.kt
+++ app/src/main/java/com/example/android/TestActivity.kt
@@ -4,19 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.android.databinding.ActivityTestBinding
 
 class TestActivity : AppCompatActivity() {
+    lateinit var bindingClass: ActivityTestBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+        bindingClass = ActivityTestBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+        val message = intent.getStringExtra("key")
+        bindingClass.tvMessage.text = message
     }
 
-    fun onClickGoMain(view: View) {
-
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-
+    fun onClickBack(view: View){
+        intent.putExtra("key2", bindingClass.edName.text.toString())
+        setResult(RESULT_OK, intent)
+        finish()
     }
-
-
 }
