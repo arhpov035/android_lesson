2d95ed28a2b11fbdc1f541f4bc1873c78ed1402c
arhpov035 <78172721+arhpov035@users.noreply.github.com>
2022-03-22 22:05:11 +0300

    Решение домашнего задания мой вариант - 21

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -6,19 +6,44 @@ import android.util.Log
 
 
 class MainActivity : AppCompatActivity() {
-    val lostArray = arrayOf(10000, 2300, 45000, 65000, 6500, 400)
-    val earnArray = arrayOf(15000, 300, 345000, 5000, 16500, 3400)
-    val resultArray = ArrayList<String>()
+    val bad = 0..3
+    val normal = 4..6
+    val nice = 7..9
+    val excellent = 10
+    val gradeArray = arrayOf(4, 7, 3, 6, 10, 2)
+    val nameArray = arrayOf("Антон", "Егор", "Маша", "Светлана", "Юля", "Семен")
+    val badArray = ArrayList<String>()
+    val normalArray = ArrayList<String>()
+    val niceArray = ArrayList<String>()
+    val excellentArray = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val names = resources.getStringArray(R.array.names)
+        for ((index, name) in nameArray.withIndex()){ // withIndex - получаем название элемента и индекс
+//            resultArray.add("Имя: $name - прибыль = ${earnArray[index] - lostArray[index]}")
+            when(gradeArray[index]){
+                in bad -> badArray.add("Плохие оценки: Ученик: $name")
+                in normal -> normalArray.add("Нормальные  оценки: Ученик: $name")
+                in nice -> niceArray.add("Хорошие оценки: Ученик: $name")
+                excellent -> excellentArray.add("Отличные оценки: Ученик: $name")
+            }
+        }
 
-        for ((index, name) in names.withIndex()){ // withIndex - получаем название элемента и индекс
-            resultArray.add("Имя: $name - прибыль = ${earnArray[index] - lostArray[index]}")
-            Log.d("MyLog", "Статистика -/- ${resultArray[index]}")
+        badArray.forEach{
+            Log.d("MyLog", it)
+        }
+        normalArray.forEach{
+            Log.d("MyLog", it)
+        }
+        niceArray.forEach{
+            Log.d("MyLog", it)
+        }
+        excellentArray.forEach{
+            Log.d("MyLog", it)
         }
     }
+
+
 
 
 }
